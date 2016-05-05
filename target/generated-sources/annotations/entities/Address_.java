fd74d31f65356d479eaa1441f87fa08835746557
jagatrp <jagatrp>
2016-05-05 18:10:35 +1000

    added webpool in netbeans

--- target/generated-sources/annotations/entities/Address_.java
+++ target/generated-sources/annotations/entities/Address_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-09T15:41:21")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-05T18:03:18")
 @StaticMetamodel(Address.class)
 public class Address_ { 
 
