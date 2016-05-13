40b6dc65e9d094edce135eb59a3fb7a2556eb9cd
SANTOSH PRAJAPATI <SANTOSH PRAJAPATI>
2016-05-13 14:50:50 +1000

    created v1

--- target/generated-sources/annotations/entities/Address_.java
+++ target/generated-sources/annotations/entities/Address_.java
@@ -4,11 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-11T18:32:31")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-06T17:08:24")
->>>>>>> jagat
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-13T14:44:42")
 @StaticMetamodel(Address.class)
 public class Address_ { 
 
--- target/generated-sources/annotations/entities/Staff_.java
+++ target/generated-sources/annotations/entities/Staff_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-11T18:32:31")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-13T14:44:42")
 @StaticMetamodel(Staff.class)
 public class Staff_ extends User_ {
 
